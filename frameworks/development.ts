29f7ad88e14beeb84a20cd6c2521ccf8f2d3952b
frankleo <1542874601@qq.com>
2025-11-06 23:09:03 +0800

    使用esbuild进行开发

--- frameworks/development.ts
+++ frameworks/development.ts
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-import { FrameworkConfigManager } from "@/frameworks/commons/FrameworkConfigManager";
-import { IOCContainer } from "@/frameworks/commons/IOCContainer";
+// import { FrameworkConfigManager } from "@/frameworks/commons/FrameworkConfigManager";
+// import { IOCContainer } from "@/frameworks/commons/IOCContainer";
 
-import { DevelopmentControllerProcess } from "@/frameworks/controllers/DevelopmentController";
+// import { DevelopmentControllerProcess } from "@/frameworks/controllers/DevelopmentController";
 
-import { EventManager } from "@/frameworks/commons/EventManager";
-import { CompilerActionService } from "@/frameworks/services/preprocess/CompilerActionService";
+// import { EventManager } from "@/frameworks/commons/EventManager";
+// import { CompilerActionService } from "@/frameworks/services/preprocess/CompilerActionService";
 
-setImmediate(async () => {
-  try {
-    await IOCContainer.get(EventManager).initialize();
-    await IOCContainer.get(FrameworkConfigManager).initialize();
-    await IOCContainer.get(CompilerActionService).cleanDestnation();
-    await IOCContainer.get(DevelopmentControllerProcess).execute();
-  } catch (error) {
-    console.log("error", error);
-    process.exit(0);
-  };
-});
+// setImmediate(async () => {
+//   try {
+//     await IOCContainer.get(EventManager).initialize();
+//     await IOCContainer.get(FrameworkConfigManager).initialize();
+//     await IOCContainer.get(CompilerActionService).cleanDestnation();
+//     await IOCContainer.get(DevelopmentControllerProcess).execute();
+//   } catch (error) {
+//     console.log("error", error);
+//     process.exit(0);
+//   };
+// });
