46b8be41fad826281b08a4dc42b90c87955b08f5
frankleo <1542874601@qq.com>
2026-04-08 11:26:46 +0800

    temp commit

--- main/server/index.ts
+++ main/server/index.ts
@@ -1,19 +1,6 @@
 import { IOCContainer } from "@/main/server/commons/Application/IOCContainer";
 import { ExpressHttpServer } from "@/main/server/commons/Application/ExpressHttpServer";
 
-declare global {
-  namespace NodeJS {
-    interface Process {
-      isClient: boolean,
-      isServer: boolean
-    }
-  }
-  interface Window {
-    seo: any,
-    content: any,
-    process: any
-  }
-};
 
 setImmediate(async () => {
   const serverApplicationInstance = IOCContainer.get(ExpressHttpServer);
