7f49c9817bd813a282d653ef086f262502950318
frankleo <1542874601@qq.com>
2026-04-09 18:37:08 +0800

    temp commit

--- library/compilation.ts
+++ library/compilation.ts
@@ -2,8 +2,8 @@
  * 工程脚本编译和制作物料阶段使用的方法
  * **/
 export { setCompileConfiguration } from "@/library/sections/compilation/setCompileConfiguration";
-export { makeHydrationResource } from "@/library/sections/compilation/makeHydrationResource";
 export { makeDehydratedResource } from "@/library/sections/compilation/makeDehydratedResource";
+export { makeHydrationResource } from "@/library/sections/compilation/makeHydrationResource";
 
 export type { ICompilationConfig } from "@/library/commons/CompilationConfigManager";
 export type { ICompileAssetsList } from "@/library/utils/filterWebpackStats";
--- library/runtime.ts
+++ library/runtime.ts
@@ -1,11 +1,11 @@
 /**
  * 服务端运行时阶段需要使用的方法
  * **/
+export { renderDehydratedResourceWithSandbox } from "@/library/sections/runtime/renderDehydratedResourceWithSandbox";
 export { setRuntimeConfiguration } from "@/library/sections/runtime/setRuntimeConfiguration";
 export { getRuntimeConfiguration } from "@/library/sections/runtime/getRuntimeConfiguration";
-export { getHydrationResource } from "@/library/sections/runtime/getHydrationResource";
 export { getDehydratedResource } from "@/library/sections/runtime/getDehydratedResource";
-export { renderDehydratedResourceWithSandbox } from "@/library/sections/runtime/renderDehydratedResourceWithSandbox";
+export { getHydrationResource } from "@/library/sections/runtime/getHydrationResource";
 
-export type { IRuntimeConfig } from "@/library/commons/RuntimeConfigManager";
 export type { ICompileAssetsList } from "@/library/utils/filterWebpackStats";
+export type { IRuntimeConfig } from "@/library/commons/RuntimeConfigManager";
